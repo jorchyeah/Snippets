fa46978ae9706999b8448dfc1d9784e2b708eb4c
Jorge Cervantes <jc.3@live.com.mx>
2020-01-04 10:52:49 -0600

    Primera prueba de formato

--- Python/ajusteFecha.py
+++ Python/ajusteFecha.py
@@ -1,6 +1,16 @@
+#este programa cambia los nombres de screenshots guardados en un directorio
 import os
 
+#cambio de directorio
 os.chdir('/Users/Jorch/Desktop/Nueva carpeta')
 
+#listado de archivos
 for f in os.listdir():
-    print(f)
+    f_nombre, f_ext = os.path.splitext(f)
+    f_screenshot, f_feCompleta = f_nombre.split('_')
+    f_fecha, f_hora = f_feCompleta.split('-')
+
+    print('{} {}{}'.format(f_fecha,f_hora,f_ext))
+
+
+
